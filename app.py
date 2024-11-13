c7fadad549ba581851d4b5cd8a2aa0bf7c10838c
Tad Nicol <tadnicol@me.com>
2024-11-12 17:34:27 -0800

    Add examples

--- app.py
+++ app.py
@@ -11,5 +11,13 @@ def predict(img):
     return {labels[i]: float(probs[i]) for i in range(len(labels))}
 
 
-iface = gr.Interface(fn=predict, inputs=gr.Image(), outputs=gr.Label(num_top_classes=7), title='Big Kitty Classifier')
+
+
+
+iface = gr.Interface(fn=predict, inputs=gr.Image(), outputs=gr.Label(num_top_classes=7), title='Big Kitty Classifier',
+                     examples=['assets/examples/Tiger.jpeg', 'assets/examples/Lion.jpeg',
+                               'assets/examples/Cheetah.jpeg', 'assets/examples/Panther.jpeg',
+                               'assets/examples/Leopard.jpeg', 'assets/examples/Snow Leopard.jpeg',
+                               'assets/examples/Cougar.jpeg'])
+
 iface.launch(share=True)
